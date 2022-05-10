509782d3a40d0f4558ded2170f6eb41d8d0806c7
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-10 19:55:03 -0300

    Format applied to model, migrations and api services files

--- app/models/user.js
+++ app/models/user.js
@@ -1,34 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define(
-      'users',
-      {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        first_name: { 
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        last_name: { 
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: { 
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        password:{ 
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+  const User = sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
       },
-      {
-        timestamps: false
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
       }
-    );
-    return User;
-  };
+    },
+    {
+      timestamps: false
+    }
+  );
+  return User;
+};
