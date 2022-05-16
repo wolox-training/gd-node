2f877086a0858b33acf27f6b80f29ea95e7e217c
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-16 17:30:00 -0300

    create test for user endpoint

---
+++ test/user.test.js
@@ -0,0 +1,34 @@
+const repository = require('../app/services/databases/user');
+const usersFactory = require('./factory/user');
+
+describe('Testing Endpoint User /POST', () => {
+  describe('', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test('User creation succesy', async () => {
+      const userToTest = {
+        first_name: 'Tom',
+        last_name: 'Lee',
+        email: 'Tom.Lee@wolox.com',
+        password: '12345rE8'
+      };
+      const result = await repository.store(userToTest);
+      expect(result).toBeInstanceOf(Object);
+    });
+
+    test('User creation fail wrong E-mail', async () => {
+      const userToTest = {
+        first_name: 'Tom',
+        last_name: 'Lee',
+        email: 'Tom.Lee@wolox.com',
+        password: '12345rE8'
+      };
+      const newUser = await usersFactory.create();
+      console.log(newUser);
+      const result = await repository.store(userToTest);
+      expect(result[1]).toBe(false);
+    });
+  });
+});
