9dc81b6cf5ee1a38841a5305100a7a5990631d20
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-16 07:50:58 -0300

    add create User test file

---
+++ test/createUser.test.js
@@ -0,0 +1,13 @@
+const createUser = require('../app/controllers/createUser');
+const { testing } = require('../config/db');
+
+describe('Test user creation', () => {
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    })
+    describe('test create user', () => {
+        testing('', () => {
+            
+        })
+    })
+})
