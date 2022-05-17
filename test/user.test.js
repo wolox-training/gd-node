1df34212b993d81111090d89f9d00c2a90c1e31e
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-17 16:01:06 -0300

    remove logs

--- test/user.test.js
+++ test/user.test.js
@@ -29,7 +29,6 @@ describe('Testing Endpoint User /POST', () => {
       };
       await usersFactory.create();
       const result = await repository.store(userToTest);
-      console.log(result, '999');
       expect(result[1]).toBe(false);
     });
     test('User creation fail wrong password', async () => {
