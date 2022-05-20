a53bab6839baaa07460378c967a7b64450921c66
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-20 08:29:00 -0300

    add test for signIn with supertest library

--- test/user.test.js
+++ test/user.test.js
@@ -1,8 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
 const repository = require('../app/services/databases/user');
 const usersFactory = require('./factory/user');
 
-describe('Testing Endpoint User /POST', () => {
-  describe('', () => {
+describe('Testing Endpoint User', () => {
+  describe('signUp', () => {
     beforeEach(async () => {
       await jest.clearAllMocks();
     });
@@ -52,6 +54,58 @@ describe('Testing Endpoint User /POST', () => {
       await usersFactory.create();
       const result = await repository.store(userToTest);
       expect(result.errors).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('signIn', () => {
+    beforeEach(async () => {
+      await jest.clearAllMocks();
+      await usersFactory.create();
+    });
+    afterEach(async () => {
+      await usersFactory.cleanUp();
+    });
+    test('User login successfully', async () => {
+      const userToTest = {
+        email: 'Tom.Lee@wolox.com',
+        password: '12345rt8'
+      };
+      await request(app)
+        .post('/users/sessions')
+        .send(userToTest)
+        .then(response => {
+          expect(response.body.email).toEqual('Tom.Lee@wolox.com');
+          expect(response.statusCode).toBe(200);
+        });
+    });
+    test('User login fail wrong password', async () => {
+      await jest.setTimeout(30000);
+      const userToTest = {
+        email: 'Tom.Lee@wolox.com',
+        password: '12345rt89'
+      };
+      await request(app)
+        .post('/users/sessions')
+        .send(userToTest)
+        .then(response => {
+          expect(response.error.text).toEqual('{"message":"Wrong email or password"}');
+          expect(response.statusCode).toBe(400);
+        });
+    });
+    test('User login fail wrong email', async () => {
+      await jest.setTimeout(30000);
+      const userToTest = {
+        email: 'Tom.Lee.13@wolox.com',
+        password: '12345rt8'
+      };
+      await request(app)
+        .post('/users/sessions')
+        .send(userToTest)
+        .then(response => {
+          console.log(response, '888');
+          expect(response.error.text).toEqual('{"message":"Wrong email or password"}');
+          expect(response.statusCode).toBe(400);
+        });
     });
   });
 });
