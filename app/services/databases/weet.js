eef9f5d73cab22c9af35dab2190f42d9405f0fba
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-26 15:51:13 -0300

    update weet create function on database

--- app/services/databases/weet.js
+++ app/services/databases/weet.js
@@ -1,9 +1,13 @@
-// const { Weet } = require('../../models');
+const { Weet } = require('../../models');
 const { giveMeJokes } = require('../externals/geekJokes');
 
-const store = async weetToCreate => {
+const store = async weetUser => {
   try {
-    const result = await giveMeJokes();
+    const myWeet = await giveMeJokes(); 
+    const result = await Weet.create({
+      content: myWeet.data.joke,
+      user_id: weetUser.decoded.id
+    });
     return result;
   } catch (error) {
     return error;
