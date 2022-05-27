ad046ede9c1d70fa527d91b4709338d86ec738f9
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-27 14:00:27 -0300

    add check 140 chars logs

--- app/services/externals/geekJokes.js
+++ app/services/externals/geekJokes.js
@@ -2,10 +2,21 @@ const axios = require('axios').default;
 
 const URL = `${process.env.WEETS_API_URL}?format=json`;
 
+function shortJoke(stringJoke) {
+  const originalJoke = stringJoke.slice(0, 139);
+  const findSpace = originalJoke.lastIndexOf(' ');
+  const finalJoke = originalJoke.slice(0, findSpace);
+  return finalJoke;
+}
+
 function giveMeJokes() {
   return axios
     .get(URL)
-    .then(response => response)
+    .then(response => {
+      const newJoke = response.data.joke;
+      const jokeToSend = newJoke.length > 140 ? shortJoke(newJoke) : newJoke;
+      return jokeToSend;
+    })
     .catch(error => error);
 }
 
