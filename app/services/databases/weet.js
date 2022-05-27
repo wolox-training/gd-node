23ac25f40fdc313ff533766e2ddc2cc6dd84d37a
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-05-27 08:53:57 -0300

    delete space

--- app/services/databases/weet.js
+++ app/services/databases/weet.js
@@ -3,7 +3,7 @@ const { giveMeJokes } = require('../externals/geekJokes');
 
 const store = async weetUser => {
   try {
-    const myWeet = await giveMeJokes(); 
+    const myWeet = await giveMeJokes();
     const result = await Weet.create({
       content: myWeet.data.joke,
       user_id: weetUser.decoded.id
