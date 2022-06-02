f5111315e997ae59b3dccb4552263f40bba9cfe5
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-02 16:45:17 -0300

    add new function to Qualification databases

--- app/services/databases/qualification.js
+++ app/services/databases/qualification.js
@@ -1,10 +1,11 @@
 const { Qualification } = require('../../models');
+const { Op } = require("sequelize");
 
 const store = async ({ findUser, findWeet, weetScore }) => {
   try {
     const result = await Qualification.create({
-      rating_user_id: findUser.id,
-      weet_id: findWeet.id,
+      rating_user_id: findUser,
+      weet_id: findWeet,
       score: weetScore
     });
     return result;
@@ -13,6 +14,56 @@ const store = async ({ findUser, findWeet, weetScore }) => {
   }
 };
 
+const getOne = async ({ findUser, findWeet }) => {
+  try {
+    const result = await Qualification.findOne({
+      where: {
+        [Op.and]: [
+          { rating_user_id: findUser },
+          { weet_id: findWeet }
+        ]
+      }
+    });
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+const update = async weetId => {
+  try {
+    console.log(findUser, findWeet);
+    const result = await Qualification.update({
+      where: {
+        id: weetId
+      }
+    });
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+const sum = async ({ findUser, findWeet }) => {
+  try {
+    const result = await Qualification.count({
+      where: {
+        [Op.and]: [
+          { rating_user_id: findUser },
+          { weet_id: findWeet }
+        ]
+        }
+    });
+    console.log(result, '999');
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
-  store
+  store,
+  getOne,
+  update,
+  sum
 };
