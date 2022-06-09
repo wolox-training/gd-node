f3dfc93e5f09a7fee0f51356353ec411d1d16376
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-09 10:19:27 -0300

    add test for congratsEmails

---
+++ test/congratsEmail.test.js
@@ -0,0 +1,15 @@
+const { job } = require('../app/services/externals/congratsEmail');
+
+describe('Testing Cron Job', () => {
+  describe('Create cron job', () => {
+    beforeEach(async () => {
+      await jest.clearAllMocks();
+    });
+
+    test('Create cron job successfully', () => {
+      const spy = jest.spyOn(job, 'start');
+      job.start();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+});
