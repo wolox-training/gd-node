cc91642c4c3d23260d493841f7a70149f0f933bd
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-10 17:04:19 -0300

    add expiration and issue at to token creation function

--- app/services/internals/getToken.js
+++ app/services/internals/getToken.js
@@ -1,6 +1,8 @@
 const jwt = require('jwt-simple');
 
 function createToken(payload) {
+  payload.iat = Math.round(Date.now() / 1000);
+  payload.exp = Math.round(Date.now() / 1000) + 3600;
   const token = jwt.encode(payload, process.env.SECRET_KEY, 'HS256');
   return token;
 }
