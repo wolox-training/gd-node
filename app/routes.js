4599d76a249afd538fafa7148ba195dbf63d77ff
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-10 10:19:38 -0300

    add auth0 to routes

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,23 @@
+const { requiresAuth } = require('express-openid-connect');
 const { healthCheck } = require('./controllers/healthCheck');
 const { validateSignUp, validateSignIn, isStandardUser, isAdminUser } = require('./middlewares/user');
 const { validateQualifyWeet } = require('./middlewares/qualification');
 const { signUp, signIn, listAllUsers, signUpOrUpdateAdmin, destroySession } = require('./controllers/user');
 const { createWeet, listAllWeets } = require('./controllers/weet');
 const { createQualifyWeet } = require('./controllers/qualifications');
+// const { checkJwt, checkScopes } = require('./services/externals/auth0');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/users', isStandardUser, listAllUsers);
   app.post('/users', validateSignUp, signUp);
+  app.get('/users/login', (req, res) => {
+    console.log(req, '999');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+  app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
   app.post('/users/sessions', validateSignIn, signIn);
   app.post('/users/sessions/invalidate_all', isStandardUser, destroySession);
   app.post('/admin/users', isAdminUser, validateSignUp, signUpOrUpdateAdmin);
