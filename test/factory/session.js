f8b8e65d01764128ae211eccbeeb56f15e5afb62
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-13 09:27:53 -0300

    update session factory

--- test/factory/session.js
+++ test/factory/session.js
@@ -4,10 +4,9 @@ const { Session } = require('../../app/models');
 factory.define('session', Session, {
   token:
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZmlyc3RfbmFtZSI6InRvbSIsImxhc3RfbmFtZSI6ImxlZSIsImVtYWlsIjoidG9tLmxlZUB3b2xveC5jb20iLCJyb2xlX2lkIjoic3RhbmRhcmQiLCJpYXQiOjE2NTQ4ODQxMzUsImV4cCI6MTY1NDg4NDE5NX0.JpTaW6KLR6Yha7FwXTh1C47girIepO1JR_zYdirYW_g',
-  token_user_id: '1',
+  token_user_id: 1,
   created_at: '2022-06-10 15:35:48.534-03',
-  updated_at: '2022-06-10 15:35:48.534-03',
-  deleted_at: ''
+  updated_at: '2022-06-10 15:35:48.534-03'
 });
 
 module.exports = {
